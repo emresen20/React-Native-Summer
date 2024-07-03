0745cde1b7ff609268df8add32e202688842313c
emresen20 <esen20@posta.pau.edu.tr>
2024-07-03 17:29:37 +0300

    quantity decrase increase aded

--- NikeApp/src/store/cardSlices.js
+++ NikeApp/src/store/cardSlices.js
@@ -25,7 +25,18 @@ export const cartSlice=createSlice({
         },
 
         changeQuantity: (state,action) =>{
-            
+            const{productId,amount}=action.payload;
+            const cartItem= state.items.find(item => item.product.id === productId);
+
+            if (cartItem){
+                cartItem.quantity +=amount
+            }
+
+            if(cartItem.quantity <= 0 ){
+                state.items=state.items.filter((item) => item !== cartItem)
+            }
+
+
         },
     }
 })

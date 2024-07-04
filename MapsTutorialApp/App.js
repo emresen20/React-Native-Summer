b36f927be5f19b9df1bade76c0e2e6acd5fb1edb
emresen20 <esen20@posta.pau.edu.tr>
2024-07-04 14:14:26 +0300

    deneme

--- MapsTutorialApp/App.js
+++ MapsTutorialApp/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useState } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import MapStyle from './MapStyle';
+
 export default function App() {
+  // Markerları saklamak için useState hook'u kullanılıyor
+  let [markers, setMarkers] = useState([
+    {
+      lat: 37.778690,
+      long: 29.061660,
+    },
+  ]);
 
   return (
     <View style={styles.container}>
       <MapView
         style={StyleSheet.absoluteFill}
-        initialRegion={{
-          latitude: 37.778690,
-          longitude: 29.061660,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        customMapStyle={MapStyle}
-        showsMyLocationButton={true}
+        customMapStyle={MapStyle} // Harita stilini belirlemek için
+        provider={PROVIDER_GOOGLE} // Google harita sağlayıcısı kullanılıyor
+        onPress={(e) =>
+          setMarkers([...markers,
+          { lat: e.nativeEvent.coordinate.latitude, long: e.nativeEvent.coordinate.longitude }])}
 
-      />
+      >
+        {/* Markerları haritaya eklemek için markers array'i map ile döngüye sokuluyor */}
+        {markers.map((e, i) => (
+          <Marker key={i} pinColor='Yellow' coordinate={{ latitude: e.lat, longitude: e.long }} />
+        ))}
+      </MapView>
     </View>
   );
 }

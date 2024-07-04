c9b5ed36eeb3e751a50394b3ca80dc8c61b7788e
emresen20 <esen20@posta.pau.edu.tr>
2024-07-04 13:51:41 +0300

    custom map added

--- MapsTutorialApp/App.js
+++ MapsTutorialApp/App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import MapStyle from './MapStyle';
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: 37.778690,
+          longitude: 29.061660,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        customMapStyle={MapStyle}
+        showsMyLocationButton={true}
+
+      />
     </View>
   );
 }
@@ -13,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });

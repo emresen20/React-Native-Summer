44ffe7f81c1b18c8797cf4a0819e7c20e2e8f549
emresen20 <esen20@posta.pau.edu.tr>
2024-07-05 14:10:45 +0300

    stack on the top

--- ShenWallet/src/components/Card.tsx
+++ ShenWallet/src/components/Card.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Animated, { useDerivedValue, useSharedValue } from 'react-native-reanimated';
+import React, { useState } from 'react'
+import Animated, { clamp, useDerivedValue, useSharedValue } from 'react-native-reanimated';
 import { View } from 'react-native-reanimated/lib/typescript/Animated'
 
 const Card = ({card,index,scrollY}) => {
-    const translateY=useDerivedValue(()=> -scrollY.value)
+
+    const [cardHeight,setCardHeight]=useState(0); //cimagenin yüksekliğinin boyutunu alldı
+
+    const translateY=useDerivedValue(()=>clamp(-scrollY.value, -index* cardHeight*0.9,0) ) //yukarıda bir stack halinde durmasını sağladık
+    //her indexi imagenin yüksekliği ile çarptık mesla ilk index 1 200 yukarı gitmesi gerek ikincisini 400 bu yü<den indexlerle çarptık 
+    //0.9 ile ise yukarıdan biraz boşluk bırakmak için yaptık
     return (
         <Animated.Image  //animmelerin çalışması için burdan çağrışım yaptık
             source={card}
+            onLayout={(event)=> setCardHeight(event.nativeEvent.layout.height+10)}
             style={{
                 width: '100%',
                 aspectRatio: 7 / 4,

36b2bb340f40e740586902e4cbac41b7bbb5e3b7
emresen20 <esen20@posta.pau.edu.tr>
2024-07-09 09:42:41 +0300

    serach and selected rooms

--- BookingApp/screens/HomeScreen.js
+++ BookingApp/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import { Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { Button, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons';
@@ -9,6 +9,11 @@ import DatePicker from 'react-native-date-ranges';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
+    const [selectedDates, setSelectedDates] = useState()
+    console.log("selectedDates" + selectedDates)
+    const [rooms, setRooms] = useState(1);
+    const [adults, setAdults] = useState(2);
+    const [children, setChildren] = useState(0);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -30,7 +35,24 @@ const HomeScreen = () => {
                 <Ionicons name="notifications-outline" size={24} color="white" style={{ marginRight: 12 }} />
             )
         })
-    })
+    });
+    const customButton = (onConfirm) => {
+        return (
+            <TouchableOpacity onPress={onConfirm}>
+                <Text style={{ color: "#003580" }}>Sumbit</Text>
+            </TouchableOpacity>
+            // <Button 
+            // onPress={onConfirm}
+            // style={{
+            //     container:{width:'80%',marginHorizontal:'3%'},
+            //     text:{fontSize:20},
+            // }}
+            // primary
+            // title='Sumbit'
+
+            // />
+        )
+    }
     return (
         <View>
             <Header />
@@ -53,7 +75,7 @@ const HomeScreen = () => {
                         borderWidth: 1
                     }}>
                         <Feather name="search" size={24} color="black" />
-                        <TextInput placeholder='Enter Your Destination' />
+                        <TextInput placeholder='Enter Your Destination' placeholderTextColor={"black"} />
                     </Pressable>
 
                     {/* Selected Dates */}
@@ -68,27 +90,74 @@ const HomeScreen = () => {
                     }} >
                         <Feather name="calendar" size={24} color="black" />
                         <DatePicker
-                            style={{ width: 350, height: 45 }}
+                            style={{
+                                width: 350,
+                                height: 30,
+                                borderRadius: 0,
+                                borderWidth: 0,
+                                borderColor: "transparent"
+                            }}
                             customStyles={{
-                                placeholderText: { fontSize: 20 } // placeHolder style
+                                placeholderText: {
+                                    fontSize: 15,
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                    marginRight: "auto"
+                                },
+                                headerStyle: {
+                                    backgroundColor: "#003580",
 
-                            }} // optional 
-                            centerAlign // optional text will align center or not
+                                },
+                                contentText:
+                                {
+                                    fontSize: 15,
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                    marginRight: "auto"
+                                },
+
+
+                            }}
+
+                            selectedBgColor="orange"
+                            customButton={(onConfirm) => customButton(onConfirm)}
+                            onConfirm={(starDate, endDate) => setSelectedDates(starDate, endDate)}
                             allowFontScaling={false} // optional
                             placeholder={'Apr 27, 2018 → Jul 10, 2018'}
                             mode={'range'}
                         />
 
-                    </Pressable>
+                    </Pressable >
 
                     {/* Rooms and Guest */}
-                    <Pressable>
+                    <Pressable style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        paddingHorizontal: 10,
+                        borderColor: "orange",
+                        paddingVertical: 13,
+                        borderWidth: 1
+                    }}>
+                        <Ionicons name="person-outline" size={24} color="black" />
+                        <TextInput placeholder='1 room 2 adults 0 children' placeholderTextColor={"red"} />
 
                     </Pressable>
 
                     {/* Search Button */}
-                    <Pressable>
-
+                    <Pressable style={{
+                        paddingHorizontal: 10,
+                        borderColor: "orange",
+                        paddingVertical: 13,
+                        borderWidth: 1,
+                        backgroundColor: "#003580"
+                    }}>
+                        <Text style={{
+                            textAlign: "center",
+                            fontSize: 15,
+                            fontWeight: "500",
+                            color: "white"
+                        }}>Search</Text>
                     </Pressable>
                 </View>
             </ScrollView>

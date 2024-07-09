e1666e17dbce0cec80b374588b8416110ad64694
emresen20 <esen20@posta.pau.edu.tr>
2024-07-09 14:57:43 +0300

    data added

--- BookingApp/screens/PlacesScreen.js
+++ BookingApp/screens/PlacesScreen.js
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useLayoutEffect, useState } from 'react'
-import { useRoute } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Octicons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { data } from '../data';
 
 const PlacesScreen = () => {
     const route = useRoute();
+    const navigation = useNavigation();
+
     useLayoutEffect(() => {
         navigation.setOptions({
             headershown: true,
-            title: "Booking.com",
+            title: "Popular Places",
             headerTitleAlign: "center",
             headerTitleStyle: {
                 fontSize: 20,
@@ -21,15 +27,78 @@ const PlacesScreen = () => {
                 height: 110,
 
             },
-            headerRight: () => (
-                <Ionicons name="notifications-outline" size={24} color="white" style={{ marginRight: 12 }} />
-            )
+
         })
     });
     console.log(route.params)
     return (
         <View>
-            <Text>PlacesScreen</Text>
+            <Pressable
+                style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    paddingHorizontal: 20,
+                    padding: 12,
+                    backgroundColor: "white"
+                }}>
+                <Pressable
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center"
+                    }}>
+                    <Octicons name="arrow-switch" size={22} color="gray" />
+                    <Text
+                        style={{
+                            fontSize: 15,
+                            fontWeight: "500",
+                            marginLeft: 8
+
+                        }}>Sort
+                    </Text>
+                </Pressable>
+
+                <Pressable
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center"
+                    }}>
+                    <Ionicons name="filter" size={22} color="gray" />
+                    <Text
+                        style={{
+                            fontSize: 15,
+                            fontWeight: "500",
+                            marginLeft: 8
+
+                        }}>
+                        Filter
+                    </Text>
+                </Pressable>
+
+                <Pressable
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center"
+                    }}>
+                    <Fontisto name="map-marker-alt" size={22} color="gray" />
+                    <Text
+                        style={{
+                            fontSize: 15,
+                            fontWeight: "500",
+                            marginLeft: 8
+
+                        }}>Map
+                    </Text>
+                </Pressable>
+
+            </Pressable>
+            <ScrollView 
+                style={{
+                    backgroundColor:"#F5F5F5"
+                }}>
+
+
+            </ScrollView>
         </View>
     )
 }

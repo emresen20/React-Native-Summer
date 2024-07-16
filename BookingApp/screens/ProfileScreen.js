39972e0f40d2562b2f9336da4fcccccb7a1fc2e4
emresen20 <esen20@posta.pau.edu.tr>
2024-07-16 11:47:43 +0300

    LogOut

--- BookingApp/screens/ProfileScreen.js
+++ BookingApp/screens/ProfileScreen.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const navigation=useNavigation();
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => navigation.navigate("LoginScreen"))
+      .catch(error => console.error(error));
+  };
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <Pressable style={styles.button} onPress={handleSignOut}>
+        <Text style={styles.buttonText}>Çıkış</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});

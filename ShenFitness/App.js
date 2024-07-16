e64ca331ea1fc81daf555b256e12ed140769ca0a
emresen20 <esen20@posta.pau.edu.tr>
2024-07-16 16:17:17 +0300

    navigatorcreated

--- ShenFitness/App.js
+++ ShenFitness/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex:1}}> 
-      <HomeScreen/>
-    </SafeAreaView>
+   <StackNavigator/>
   );
 }
 

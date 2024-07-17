7632136d632f9a6e1e144c77d300269e0ad375da
emresen20 <esen20@posta.pau.edu.tr>
2024-07-17 17:52:28 +0300

    rest screen created

--- ShenFitness/StackNavigator.js
+++ ShenFitness/StackNavigator.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import WorkOutScreen from './screens/WorkOutScreen';
 import FitScreen from './screens/FitScreen';
+import RestScreen from './screens/RestScreen';
 
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ const StackNavigator = () => {
                 <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown:false}} />
                 <Stack.Screen name="WorkOutScreen" component={WorkOutScreen} options={{headerShown:false}} />
                 <Stack.Screen name="FitScreen" component={FitScreen} options={{headerShown:false}} />
+                <Stack.Screen name="RestScreen" component={RestScreen} options={{headerShown:false}} />
             </Stack.Navigator>
         </NavigationContainer>
     )

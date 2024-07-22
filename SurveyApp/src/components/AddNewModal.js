e2ff591e04960ec3e7d471794e123f3637ed7971
emresen20 <esen20@posta.pau.edu.tr>
2024-07-22 09:39:45 +0300

    modal statechange

--- SurveyApp/src/components/AddNewModal.js
+++ SurveyApp/src/components/AddNewModal.js
@@ -1,24 +1,66 @@
 import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { AntDesign } from '@expo/vector-icons';
 
 const AddNewModal = () => {
+    const [questions,setQuestions]=useState("")
+    const [options,setOptions]=useState([{text:""}, {text:""}])
+
+    const handleOptionChange=(val,i)=>{
+        const data = [...options];
+        data[i].text=val;
+        setOptions(data)
+    }
+
+    const handleNewOptions = ()=>{
+        if(options.length>=5){
+            return;
+        }
+        setOptions((prev)=> [...prev ,{text:""}])
+    }
+
+    const handleSumbit= ()=>{
+        console.log("Questions",questions)
+        console.log("Options",options)
+    }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.QuestionsText}>Question</Text>
-      <TextInput style={styles.textinput} placeholder='Enter a new question' />
-
+      <TextInput
+       style={styles.textinput} 
+       placeholder='Enter a new question' 
+       value={questions}
+       onChangeText={setQuestions}
+       />
+       
       <Text style={styles.OptionsText}>Options</Text>
-      <TextInput style={styles.textinput} placeholder='Enter a new question' />
-      <TextInput style={styles.textinput} placeholder='Enter a new question' />
+      {
+        options.map((item,i)=>(
+            <TextInput 
+            style={styles.textinput} 
+            placeholder='Enter a new question' 
+            value={item.text}
+            key={i}
+            onChangeText={(val)=> handleOptionChange(val,i)}
+            />
+        ))
+      }
+     
+      
       <Pressable style={styles.pressablecontainer}>
         
-        <Pressable style={{padding:12,backgroundColor:"gray",borderRadius:5}}>
+        <Pressable 
+            onPress={handleNewOptions}
+            disabled={options.length>=5}
+            style={{padding:12,backgroundColor:"gray",borderRadius:5}}>
+            
             <AntDesign name="pluscircleo" size={30} color="white" />
 
         </Pressable>
       </Pressable>
-      <Pressable style={{backgroundColor:"lightblue",position:"absolute",bottom:0,width:"100%",padding:12,}}>
+      <Pressable 
+            onPress={handleSumbit}
+            style={{backgroundColor:"lightblue",position:"absolute",bottom:0,width:"100%",padding:12,}}>
             <Text style={{color:"white",textAlign:"center",fontWeight:"bold",fontSize:18}}>Save</Text>
         </Pressable>
     </SafeAreaView>

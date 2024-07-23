43e5e863da86fa1aff61918c9ecdcd3d9543ace3
emresen20 <esen20@posta.pau.edu.tr>
2024-07-23 09:18:20 +0300

    progresbar

--- SurveyApp/src/components/Resultsitem.js
+++ SurveyApp/src/components/Resultsitem.js
@@ -1,14 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import * as Progress from 'react-native-progress';
 
 const Resultsitem = ({item,total}) => {
+  const percentage = (item.answers_aggregate.aggregate.count * 100) / total;
+  const progress = percentage / 100;
   return (
     <View style={{margin:10}}>
       <Text>
-        {item.text} (%{(((item.answers_aggregate.aggregate.count *100)/total)).toFixed(1)})
+        {item.text} (%{percentage.toFixed(1)})
         {"-"}
         {item.answers_aggregate.aggregate.count}
         </Text>
+        <Progress.Bar progress={progress} width={300} height={18}  style={{marginTop:5}}/>
     </View>
   )
 }

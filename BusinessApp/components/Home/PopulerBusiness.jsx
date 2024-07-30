716775080d6abbd2fc7b27bfb7dca6f8d5a0f08f
emresen20 <esen20@posta.pau.edu.tr>
2024-07-30 13:14:29 +0300

    popularbusiness

---
+++ BusinessApp/components/Home/PopulerBusiness.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PopulerBusiness = () => {
+  return (
+    <View>
+      <Text>PopulerBusiness</Text>
+    </View>
+  )
+}
+
+export default PopulerBusiness
+
+const styles = StyleSheet.create({})

ccde1cb92a2e699bc1583409ac7c19893cdc2a6e
emresen20 <esen20@posta.pau.edu.tr>
2024-08-01 11:12:18 +0300

    rating with comment

--- BusinessApp/components/BusinessDetail/Reviews.jsx
+++ BusinessApp/components/BusinessDetail/Reviews.jsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { Rating } from 'react-native-ratings'
+import { Colors } from "../../constants/Colors"
 
-const Reviews = ({business}) => {
+const Reviews = ({ business }) => {
 
-    const [rating,setRating]=useState(4)
+    const [rating, setRating] = useState(4)
+    const [userInput,setUserInput]=useState();
+    
     return (
         <View style={styles.container}>
             <Text style={styles.reviewstext}>
@@ -12,10 +15,28 @@ const Reviews = ({business}) => {
             </Text>
             <View>
                 <Rating
+                    imageSize={25}
                     showRating={false}
-                    onFinishRating={(rating)=>setRating(rating)}
+                    onFinishRating={(rating) => setRating(rating)}
                     style={{ paddingVertical: 10 }}
                 />
+                <TextInput
+                    numberOfLines={4} //alanının başlangıçta 4 satır yüksekliğinde olmasını sağlar.
+                    placeholder='Write Your Comment'
+                    onChangeText={(val)=>setUserInput(val)}
+                    value={userInput}
+                    style={styles.textinput}
+                />
+                <TouchableOpacity 
+                onPress={()=>console.log(userInput,rating)}
+                disabled={!userInput}
+                style={styles.sumbitbutton}>
+                    <Text
+                        style={styles.sumbittext}
+                    >
+                        Sumbit
+                    </Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -28,8 +49,27 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: "white"
     },
-    reviewstext:{
+    reviewstext: {
+        fontFamily: "outfit-bold",
+        fontSize: 18
+    },
+    textinput: {
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        borderColor: Colors.PRIMARY,
+
+    },
+    sumbitbutton: {
+        padding: 10,
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 6,
+        marginTop:10,
+    },
+    sumbittext:{
         fontFamily:"outfit-bold",
-        fontSize:18
+        color:"white",
+        textAlign:"center",
+        fontSize:15
     }
 })

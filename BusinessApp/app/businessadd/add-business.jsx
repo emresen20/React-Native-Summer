b81806936438d488c360b1331e3b0f9c6500beb3
emresen20 <esen20@posta.pau.edu.tr>
2024-08-02 15:11:16 +0300

    business add

---
+++ BusinessApp/app/businessadd/add-business.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddBusinesss = () => {
+  return (
+    <View>
+      <Text>AddBusinesss</Text>
+    </View>
+  )
+}
+
+export default AddBusinesss
+
+const styles = StyleSheet.create({})
--- BusinessApp/components/Profile/MenuList.jsx
+++ BusinessApp/components/Profile/MenuList.jsx
@@ -1,15 +1,18 @@
-import { FlatList, StyleSheet, Text, View,Image } from 'react-native'
+import { FlatList, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Colors } from '../../constants/Colors'
+import { useRouter } from 'expo-router'
 
 const MenuList = () => {
+
+    const router= useRouter();
 
     const menuList = [
         {
             id: 1,
             name: "Add Business",
             icon: require("../../assets/images/add.png"),
-            path: ''
+            path: "/businessadd/add-business"
         },
         {
             id: 2,
@@ -32,18 +35,21 @@ const MenuList = () => {
     ]
 
     return (
-        <View style={{marginTop:40}}>
+        <View style={{ marginTop: 40 }}>
             <FlatList
                 numColumns={2}
                 data={menuList}
                 renderItem={({ item, index }) => (
-                    <View style={styles.businessView}>
+                    <TouchableOpacity
+                        onPress={()=> router.push(item.path)}
+                        key={index}
+                        style={styles.businessView}>
                         <Image
                             source={item.icon}
                             style={styles.imagestyle}
                         />
                         <Text style={styles.nameStyle}>{item.name}</Text>
-                    </View>
+                    </TouchableOpacity>
                 )}
 
             />
@@ -55,32 +61,32 @@ const MenuList = () => {
 export default MenuList
 
 const styles = StyleSheet.create({
-    imagestyle:{
-        width:50,
-        height:50
+    imagestyle: {
+        width: 50,
+        height: 50
     },
-    nameStyle:{
-        fontFamily:"outfit-medium",
-        fontSize:16,
-        flex:1,
-        color:Colors.PRIMARY
+    nameStyle: {
+        fontFamily: "outfit-medium",
+        fontSize: 16,
+        flex: 1,
+        color: Colors.PRIMARY
     },
-    businessView:{
-        flexDirection:"row",
-        alignItems:"center",
-        gap:10,
-        flex:1,
-        padding:10,
-        borderWidth:1.5,
-        borderColor:Colors.PRIMARY,
-        borderRadius:15,
-        margin:10,
-        backgroundColor:"white"
+    businessView: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+        flex: 1,
+        padding: 10,
+        borderWidth: 1.5,
+        borderColor: Colors.PRIMARY,
+        borderRadius: 15,
+        margin: 10,
+        backgroundColor: "white"
     },
-    developstyle:{
-        fontFamily:"outfit",
-        textAlign:"center",
-        marginTop:50,
-        color:Colors.ICON_BG
+    developstyle: {
+        fontFamily: "outfit",
+        textAlign: "center",
+        marginTop: 50,
+        color: Colors.ICON_BG
     }
 })

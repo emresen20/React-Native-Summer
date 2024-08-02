1fbcb3860408abc577bdc4668b81016eb5d484cc
emresen20 <esen20@posta.pau.edu.tr>
2024-08-02 11:05:38 +0300

    profile scrren

--- BusinessApp/app/(tabs)/profile.jsx
+++ BusinessApp/app/(tabs)/profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const profile = () => {
@@ -10,3 +10,5 @@ const profile = () => {
 }
 
 export default profile
+
+const styles = StyleSheet.create({})

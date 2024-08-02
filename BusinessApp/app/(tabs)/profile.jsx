53b735b53bbe95a101d641f7b07fbbbce9a0e82e
emresen20 <esen20@posta.pau.edu.tr>
2024-08-02 11:09:44 +0300

    UserInfo created

--- BusinessApp/app/(tabs)/profile.jsx
+++ BusinessApp/app/(tabs)/profile.jsx
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import UserInfo from '../../components/Profile/UserInfo'
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.container}>
+      <Text style={styles.profiletext}>
+        Profile
+        </Text>
+        <UserInfo/>
     </View>
   )
 }
 
 export default profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    padding:20,
+  },
+  profiletext:{
+    fontFamily:"outfit-bold",
+    fontSize:35
+  }
+})

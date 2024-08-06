67c57eb9464f2f6bab78099fd4960c38fda2650d
emresen20 <esen20@posta.pau.edu.tr>
2024-08-06 09:21:02 +0300

    ketboardAwareScrollView

--- BusinessApp/app/businessadd/add-business.jsx
+++ BusinessApp/app/businessadd/add-business.jsx
@@ -8,6 +8,7 @@ import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firesto
 import { db, storage } from '../../config/FirebaseConfig';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { useUser } from '@clerk/clerk-expo';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const AddBusinesss = () => {
 
@@ -62,21 +63,21 @@ const AddBusinesss = () => {
 
     const onAddNewBusiness = async () => {
         setLoading(true);
-    
+
         try {
             const fileName = Date.now().toString() + ".jpg";
             const resp = await fetch(imageUri);
             const blob = await resp.blob();
-    
+
             const imageRef = ref(storage, "business/" + fileName);
-            
+
             await uploadBytes(imageRef, blob);
             console.log("File Uploaded...");
-    
+
             const downloadUrl = await getDownloadURL(imageRef);
             console.log(downloadUrl);
             await saveBusinessDetail(downloadUrl);
-            
+
             ToastAndroid.show("New Business Added", ToastAndroid.LONG);
         } catch (error) {
             console.log("Error uploading file or saving business details:", error);
@@ -85,7 +86,7 @@ const AddBusinesss = () => {
             setLoading(false);
         }
     }
-    
+
 
     const saveBusinessDetail = async (imageurl) => {
         await setDoc(doc(db, "BusinessList", Date.now().toString()), {
@@ -101,151 +102,155 @@ const AddBusinesss = () => {
             imageUrl: imageurl
         })
         setLoading(false)
-        ToastAndroid.show("new Business",ToastAndroid.LONG)
+        ToastAndroid.show("new Business", ToastAndroid.LONG)
 
     }
 
 
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.textbusiness}>Add New Businesss</Text>
-            <Text style={styles.textfill}>Fill all details in order to add new business</Text>
-            <TouchableOpacity
-                style={{ marginTop: 20 }}
-                onPress={() => onImagePick()}
+        <KeyboardAwareScrollView>
+            <View style={styles.container}>
+                <Text style={styles.textbusiness}>Add New Businesss</Text>
+                <Text style={styles.textfill}>Fill all details in order to add new business</Text>
+                <TouchableOpacity
+                    style={{ marginTop: 20 }}
+                    onPress={() => onImagePick()}
 
-            >
-                {!imageUri ?
-                    <Image
-                        source={require("../../assets/images/placeholder.png")}
-                        style={styles.imagecamera}
-                    /> :
-                    <Image
-                        source={{ uri: imageUri }}
-                        style={styles.imagecamera2}
+                >
+                    {!imageUri ?
+                        <Image
+                            source={require("../../assets/images/placeholder.png")}
+                            style={styles.imagecamera}
+                        /> :
+                        <Image
+                            source={{ uri: imageUri }}
+                            style={styles.imagecamera2}
+                        />
+                    }
+
+                </TouchableOpacity>
+
+                <View>
+                    <TextInput
+                        placeholder='Name'
+                        onChangeText={(v) => setName(v)}
+                        placeholderTextColor={Colors.ICON_BG}
+                        
+                        style={{
+                            padding: 8,
+                            borderWidth: 1,
+                            borderRadius: 5,
+                            fontSize: 17,
+                            backgroundColor: "white",
+                            marginTop: 10,
+                            borderColor: Colors.PRIMARY,
+                            fontFamily: "outfit-bold",
+                            color:Colors.PRIMARY
+
+                        }}
                     />
-                }
+                    <TextInput
+                        placeholder='Adress'
+                        onChangeText={(v) => setAdress(v)}
+                        placeholderTextColor={Colors.ICON_BG}
+                        style={{
+                            padding: 8,
+                            borderWidth: 1,
+                            borderRadius: 5,
+                            fontSize: 17,
+                            backgroundColor: "white",
+                            marginTop: 10,
+                            borderColor: Colors.PRIMARY,
+                            fontFamily: "outfit-bold",
+                            color:Colors.PRIMARY
 
-            </TouchableOpacity>
+                        }}
+                    />
+                    <TextInput
+                        placeholder='Contact'
+                        onChangeText={(v) => setContact(v)}
+                        placeholderTextColor={Colors.ICON_BG}
+                        style={{
+                            padding: 8,
+                            borderWidth: 1,
+                            borderRadius: 5,
+                            fontSize: 17,
+                            backgroundColor: "white",
+                            marginTop: 10,
+                            borderColor: Colors.PRIMARY,
+                            fontFamily: "outfit-bold",
+                            color:Colors.PRIMARY
 
-            <View>
-                <TextInput
-                    placeholder='Name'
-                    onChangeText={(v) => setName(v)}
-                    placeholderTextColor={Colors.ICON_BG}
-                    style={{
-                        padding: 8,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        fontSize: 17,
-                        backgroundColor: "white",
-                        marginTop: 10,
-                        borderColor: Colors.PRIMARY,
-                        fontFamily: "outfit-bold",
-                        color: "red"
+                        }}
+                    />
+                    <TextInput
+                        placeholder='Website'
+                        onChangeText={(v) => setWebsite(v)}
+                        placeholderTextColor={Colors.ICON_BG}
+                        style={{
+                            padding: 8,
+                            borderWidth: 1,
+                            borderRadius: 5,
+                            fontSize: 17,
+                            backgroundColor: "white",
+                            marginTop: 10,
+                            borderColor: Colors.PRIMARY,
+                            fontFamily: "outfit-bold",
+                            color:Colors.PRIMARY
 
-                    }}
-                />
-                <TextInput
-                    placeholder='Adress'
-                    onChangeText={(v) => setAdress(v)}
-                    placeholderTextColor={Colors.ICON_BG}
-                    style={{
-                        padding: 8,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        fontSize: 17,
-                        backgroundColor: "white",
-                        marginTop: 10,
-                        borderColor: Colors.PRIMARY,
-                        fontFamily: "outfit-bold",
-                        color: "red"
+                        }}
+                    />
+                    <TextInput
+                        placeholder='About'
+                        onChangeText={(v) => setAbout(v)}
+                        multiline
+                        numberOfLines={5}
+                        placeholderTextColor={Colors.ICON_BG}
+                        style={{
+                            padding: 8,
+                            borderWidth: 1,
+                            borderRadius: 5,
+                            fontSize: 17,
+                            backgroundColor: "white",
+                            marginTop: 10,
+                            borderColor: Colors.PRIMARY,
+                            fontFamily: "outfit-bold",
+                            color:Colors.PRIMARY,
+                            height: 100
 
-                    }}
-                />
-                <TextInput
-                    placeholder='Contact'
-                    onChangeText={(v) => setContact(v)}
-                    placeholderTextColor={Colors.ICON_BG}
-                    style={{
-                        padding: 8,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        fontSize: 17,
-                        backgroundColor: "white",
-                        marginTop: 10,
-                        borderColor: Colors.PRIMARY,
-                        fontFamily: "outfit-bold",
-                        color: "red"
+                        }}
+                    />
+                </View>
 
-                    }}
-                />
-                <TextInput
-                    placeholder='Website'
-                    onChangeText={(v) => setWebsite(v)}
-                    placeholderTextColor={Colors.ICON_BG}
-                    style={{
-                        padding: 8,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        fontSize: 17,
-                        backgroundColor: "white",
-                        marginTop: 10,
-                        borderColor: Colors.PRIMARY,
-                        fontFamily: "outfit-bold",
-                        color: "red"
+                <View style={styles.imagePickerView}>
+                    <RNPickerSelect
+                    
+                        style={styles.rnpickerstyle}
+                        onValueChange={(value) => setCategory(value)}
+                        items={categoryList}
+                    />
+                </View>
+                <TouchableOpacity
 
-                    }}
-                />
-                <TextInput
-                    placeholder='About'
-                    onChangeText={(v) => setAbout(v)}
-                    multiline
-                    numberOfLines={5}
-                    placeholderTextColor={Colors.ICON_BG}
-                    style={{
-                        padding: 8,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        fontSize: 17,
-                        backgroundColor: "white",
-                        marginTop: 10,
-                        borderColor: Colors.PRIMARY,
-                        fontFamily: "outfit-bold",
-                        color: "red",
-                        height: 100
+                    onPress={() => onAddNewBusiness()}
+                    disabled={loading}
+                    style={styles.buttonstyle}>
 
-                    }}
-                />
+                    {loading ?
+                        <ActivityIndicator size={'large'} color={"white"} /> :
+                        <Text style={{
+                            textAlign: "center",
+                            fontFamily: "outfit-medium",
+                            color: "white"
+                        }}>
+                            Add New Business
+                        </Text>
+                    }
+
+                </TouchableOpacity>
             </View>
-
-            <View style={styles.imagePickerView}>
-                <RNPickerSelect
-                    style={styles.rnpickerstyle}
-                    onValueChange={(value) => setCategory(value)}
-                    items={categoryList}
-                />
-            </View>
-            <TouchableOpacity
-
-                onPress={() => onAddNewBusiness()}
-                disabled={loading}    
-                style={styles.buttonstyle}>
-
-                {loading ?
-                    <ActivityIndicator size={'large'} color={"white"}/> :
-                    <Text style={{
-                        textAlign: "center",
-                        fontFamily: "outfit-medium",
-                        color: "white"
-                    }}>
-                        Add New Business
-                    </Text>
-                }
-
-            </TouchableOpacity>
-        </View>
+        </KeyboardAwareScrollView>
     )
 }
 

13a2f244bac10aee9a1a937f2bedfdc8e375e3a7
emresen20 <esen20@posta.pau.edu.tr>
2024-08-07 17:56:06 +0300

    scrollview

--- ReduxTurkce/src/screnns/HomeScreen.js
+++ ReduxTurkce/src/screnns/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { collection, addDoc, getDocs, setDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
@@ -6,14 +6,14 @@ import { db } from '../../firebaseConfig';
 const HomeScreen = () => {
 
   const [data, setData] = useState([])
-  const [isSaved,setISSaved]=useState(false)
-  const [updateThedata,setUpdateThedata]=useState('')
+  const [isSaved, setISSaved] = useState(false)
+  const [updateThedata, setUpdateThedata] = useState('')
 
   console.log(data)
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[isSaved])
+  }, [isSaved])
   // datayı firebaseye yollama
   const sendData = async () => {
     try {
@@ -32,52 +32,52 @@ const HomeScreen = () => {
 
   const getData = async () => {
 
-    const allData=[]
+    const allData = []
 
-    try{
+    try {
       const querySnapshot = await getDocs(collection(db, "reactnativelesson"));
       querySnapshot.forEach((doc) => {
         //console.log(`${doc.id} => ${doc.data()}`);
         //setData([...data, doc.data()]) // daha önceki veriler dursun bir öncekini sonuna ekle
         //allData.push(doc.data()) // bu da şirketlerde yapılan şık çözüm
-        allData.push({...doc.data(), id:doc.id}) // idiyi dahil edip spesefik işlem yapmak
+        allData.push({ ...doc.data(), id: doc.id }) // idiyi dahil edip spesefik işlem yapmak
       });
       setData(allData)
 
-    }catch(error){
+    } catch (error) {
       console.log(error)
 
     }
-   
+
   }
 
   //datayı silme
 
   const deleteDAta = async (item) => {
-    try{
+    try {
       await deleteDoc(doc(db, "reactnativelesson", item))
       console.log("silindi başarılı")
       setISSaved(!isSaved)
 
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
-    
+
   }
 
   // update yapma
 
   const updateData = async (item) => {
     try {
-      const lessonData = doc(db, "reactnativelesson",item);
+      const lessonData = doc(db, "reactnativelesson", item);
 
       await updateDoc(lessonData, {
         content: updateThedata
       },
-  
-    );
-    console.log("güncenlendi")
-    setISSaved(!isSaved)
+
+      );
+      console.log("güncenlendi")
+      setISSaved(!isSaved)
     } catch (error) {
       console.log(error)
     }
@@ -102,52 +102,54 @@ const HomeScreen = () => {
 
   const { isLoading, user } = useSelector((state) => state.user)
   return (
-    <SafeAreaView style={{
-      flex: 1, justifyContent: "center", alignItems: "center",
-      backgroundColor: "white"
+    <ScrollView style={{
+      flex: 1,
     }}>
-    <TextInput
-      value={updateThedata}
-      onChangeText={setUpdateThedata}
-      placeholder='enter your data'
-      style={{
-        borderWidth:1,
-        width:'50%',
-        paddingVertical:10,
-        marginBottom:10
-      }}
-      />
-    <View style={{marginBottom:50}}>
-    {data.map((item, index) => (
-        <TouchableOpacity key={index} 
-        //onPress={() => deleteDAta(item.id)}
-        onPress={()=> updateData(item.id)}
-        >
-          <Text >{item?.content}</Text>
-          <Text>{item?.id}</Text>
+      <View style={{alignItems:"center",justifyContent:"center",marginTop:25}}>
+        <TextInput
+          value={updateThedata}
+          onChangeText={setUpdateThedata}
+          placeholder='enter your data'
+          style={{
+            borderWidth: 1,
+            width: '50%',
+            paddingVertical: 10,
+            marginBottom: 10
+          }}
+        />
+        <View style={{ marginBottom: 50 }}>
+          {data.map((item, index) => (
+            <TouchableOpacity key={index}
+              //onPress={() => deleteDAta(item.id)}
+              onPress={() => updateData(item.id)}
+            >
+              <Text >{item?.content}</Text>
+              <Text>{item?.id}</Text>
+            </TouchableOpacity>
+
+          ))}
+        </View>
+
+
+
+        <TouchableOpacity onPress={() => { sendData(), setISSaved(!isSaved) }}>
+          <Text>save</Text>
+        </TouchableOpacity >
+
+        <TouchableOpacity style={{ marginTop: 100 }} onPress={() => getData()}>
+          <Text>çek</Text>
         </TouchableOpacity>
-        
-      ))}
-    </View>
-   
-    
-     
-      <TouchableOpacity onPress={() => {sendData(),setISSaved(!isSaved)}}>
-        <Text>save</Text>
-      </TouchableOpacity >
 
-      <TouchableOpacity style={{ marginTop: 100 }} onPress={() => getData()}>
-        <Text>çek</Text>
-      </TouchableOpacity>
 
-      
-      <TouchableOpacity style={{ marginTop: 100 }} onPress={() => deleteDAta()}>
-        <Text>Delete data</Text>
-        <TouchableOpacity style={{ marginTop: 100 }} onPress={() => updateData()}>
-        <Text>updateData</Text>
-      </TouchableOpacity>
-      </TouchableOpacity>
-    </SafeAreaView>
+        <TouchableOpacity style={{ marginTop: 100 }} onPress={() => deleteDAta()}>
+          <Text>Delete data</Text>
+          <TouchableOpacity style={{ marginTop: 100 }} onPress={() => updateData()}>
+            <Text>updateData</Text>
+          </TouchableOpacity>
+        </TouchableOpacity>
+      </View>
+
+    </ScrollView>
   )
 }
 

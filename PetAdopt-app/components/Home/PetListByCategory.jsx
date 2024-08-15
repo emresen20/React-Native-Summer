97a2f57e34565cfae1c539c166f1fb78d71e63b4
emresen20 <esen20@posta.pau.edu.tr>
2024-08-15 09:50:31 +0300

    display pet list item

--- PetAdopt-app/components/Home/PetListByCategory.jsx
+++ PetAdopt-app/components/Home/PetListByCategory.jsx
@@ -4,10 +4,12 @@ import Category from './Category'
 import { collection, getDocs, query, where } from 'firebase/firestore'
 import { db } from '../../config/FirebaseConfig'
 import PetListItem from './PetListItem'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const PetListByCategory = () => {
 
   const [petList,setPetList]=useState([])
+  const [loading,setLoading]=useState(false)
   
   useEffect(()=>{
     GetPetList("Birds")
@@ -18,12 +20,15 @@ const PetListByCategory = () => {
 
 
   const GetPetList=async(category)=>{
+    setLoading(true)
+    setPetList([])
     const q=query(collection(db,'Pets'),where("category",'==',category))
     const querySnapshot=await getDocs(q)
 
     querySnapshot.forEach(doc=>{
       setPetList(petList=>[...petList,doc.data()])
     })
+    setLoading(false)
 
   }
 
@@ -31,6 +36,10 @@ const PetListByCategory = () => {
     <View>
       <Category category={(category)=>GetPetList(category)}/>
       <FlatList
+      style={{marginTop:hp("1%")}}
+      horizontal
+      refreshing={loading}
+      onRefresh={()=>GetPetList("Birds")}
       data={petList}
       renderItem={({item,index})=>(
         <PetListItem pet={item}/>
--- PetAdopt-app/components/Home/PetListItem.jsx
+++ PetAdopt-app/components/Home/PetListItem.jsx
@@ -1,16 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Colors from '../../constants/Colors';
 
 const PetListItem = ({pet}) => {
 
     console.log(pet)
   return (
-    <View>
-      <Text>PetListItem</Text>
+    <View style={styles.contanier}>
+      <Image 
+      source={{uri:pet.imageUrl}}
+      style={styles.imagestyle}
+      
+      />
+      <Text style={styles.nametext}>{pet.name}</Text>
+      <View style={styles.breedcontainer}>
+        <Text style={styles.breedtext}>{pet.breed}</Text>
+        <Text style={styles.agetext}>{pet.age} YRS</Text>
+      </View>
     </View>
   )
 }
 
 export default PetListItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  contanier:{
+    padding:hp("1%"),
+    marginRight:hp("1%"),
+    backgroundColor:"white",
+    borderRadius:10
+  },
+  imagestyle:{
+    width:hp("23%"),
+    height:hp("20%"),
+    objectFit:"cover",
+    borderRadius:10
+  },
+  nametext:{
+    fontFamily:"outfit-medium",
+    fontSize:hp("2%")
+    },
+    breedtext:{
+      color:Colors.GRAY,
+      fontFamily:"outfit"
+    },
+    agetext:{
+      fontFamily:"outfit",
+      color:Colors.PRIMARY,
+      backgroundColor:Colors.Second,
+      paddingHorizontal:7,
+      borderRadius:10,
+      fontSize:hp("1.6%")
+    },
+    breedcontainer:{
+      flexDirection:"row",
+      justifyContent:"space-between",
+      alignItems:"center"
+    }
+})

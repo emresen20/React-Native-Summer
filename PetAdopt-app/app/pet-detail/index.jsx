bd09626a874ed744de6f709b7fe4ff8cbffea4e2
emresen20 <esen20@posta.pau.edu.tr>
2024-08-15 18:11:27 +0300

    Adopt button

--- PetAdopt-app/app/pet-detail/index.jsx
+++ PetAdopt-app/app/pet-detail/index.jsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect } from 'react'
 import { useLocalSearchParams, useNavigation } from 'expo-router'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -6,6 +6,7 @@ import PetInfo from '../../components/PetDetails/PetInfo';
 import PetSubInfo from '../../components/PetDetails/PetSubInfo';
 import PetAbout from '../../components/PetDetails/PetAbout';
 import PetOwner from '../../components/PetDetails/PetOwner';
+import Colors from '../../constants/Colors';
 
 const PetDetails = () => {
   const pet = useLocalSearchParams() // coming from pet listiem router 
@@ -28,13 +29,21 @@ const PetDetails = () => {
 
         {/* About  */}
         <PetAbout pet={pet} />
-        
+
         {/* Pet Owner */}
-        <PetOwner pet={pet}/>
-        <View style={{height:hp('8%')}}></View>
+        <PetOwner pet={pet} />
+
+        <View style={{ height: hp('8%') }}></View>
 
       </ScrollView>
-      {/* Adopt me */}
+
+      {/* Adopt me button */}
+      <View style={styles.buttomcontainer}>
+        <TouchableOpacity style={styles.adoptButton}>
+          <Text style={styles.adopttext}>Adpot Me</Text>
+        </TouchableOpacity>
+      </View>
+
 
     </View>
   )
@@ -42,4 +51,20 @@ const PetDetails = () => {
 
 export default PetDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  adoptButton: {
+    padding: hp('2%'),
+    backgroundColor: Colors.PRIMARY
+  },
+  buttomcontainer: {
+    position:"absolute",
+    width:'100%',
+    bottom:hp('0%')
+
+  },
+  adopttext: {
+    textAlign: "center",
+    fontFamily: "outfit-medium",
+    fontSize: hp("2%")
+  }
+})

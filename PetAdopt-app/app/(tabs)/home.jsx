cf4a0efc3611c61fba64253070678689f658d6a8
emresen20 <esen20@posta.pau.edu.tr>
2024-08-15 10:18:40 +0300

    Add Button

--- PetAdopt-app/app/(tabs)/home.jsx
+++ PetAdopt-app/app/(tabs)/home.jsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import Slider from '../../components/Home/Slider';
 import PetListByCategory from '../../components/Home/PetListByCategory';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Colors from '../../constants/Colors';
 
 const Home = () => {
   return (
@@ -17,6 +19,12 @@ const Home = () => {
       <PetListByCategory/>
 
       {/* Add New Pet Option */}
+      <TouchableOpacity 
+      
+      style={styles.addbutton}>
+        <Text style={styles.addText}>Add New Pet</Text>
+        <MaterialIcons name="pets" size={24} color={Colors.PRIMARY} />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -27,5 +35,23 @@ const styles = StyleSheet.create({
   container:{
     padding:hp("3%"),
     marginTop:hp("2.9%")
+  },
+  addbutton:{
+    flexDirection:"row",
+    gap:hp("3%"),
+    alignItems:"center",
+    padding:hp("2%"),
+    marginTop:hp("1%"),
+    backgroundColor:Colors.Second,
+    borderWidth:1,
+    borderColor:Colors.PRIMARY,
+    borderRadius:15,
+    borderStyle:"dashed",
+    justifyContent:"center"
+  },
+  addText:{
+    fontFamily:"outfit-medium",
+    color:Colors.PRIMARY,
+    fontSize:hp("2%")
   }
 })

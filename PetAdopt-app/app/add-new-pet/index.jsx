7ac46b30be848609ba1e81dc8d5093b2f9965316
emresen20 <esen20@posta.pau.edu.tr>
2024-08-19 17:04:16 +0300

    pet image text

--- PetAdopt-app/app/add-new-pet/index.jsx
+++ PetAdopt-app/app/add-new-pet/index.jsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Colors from '../../constants/Colors';
 
 const AddNewPet = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: "Add New Pet"
+    })
+  }, [])
   return (
-    <View>
-      <Text>AddNewPet</Text>
+    <View style={styles.container}>
+      <Text style={styles.addnewText}>Add New Pet for adption</Text>
+      <View style={styles.imageview}>
+        <Image
+          source={require("../../assets/images/paws.png")}
+          style={styles.pawimage}
+        />
+      </View>
+      <View>
+        <Text>Pet Name*</Text>
+        <TextInput/>
+      </View>
+
     </View>
   )
 }
 
 export default AddNewPet
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: hp('2.5%')
+  },
+  addnewText: {
+    fontFamily: "outfit-medium",
+    fontSize: hp('2.3%')
+  },
+  pawimage: {
+    width: hp('8%'),
+    height: hp('8s%'),
+    borderRadius: 15,
+
+
+  },
+  imageview: {
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: hp('12%'),
+    borderRadius: 15,
+    height: hp('12%'),
+    borderColor: Colors.GRAY
+  }
+})

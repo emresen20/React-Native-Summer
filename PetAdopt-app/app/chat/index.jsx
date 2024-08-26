546c40f824648a9c81add448d3efc481735c6df7
emresen20 <esen20@posta.pau.edu.tr>
2024-08-26 12:04:50 +0300

    get user detail for chat

--- PetAdopt-app/app/chat/index.jsx
+++ PetAdopt-app/app/chat/index.jsx
@@ -1,7 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../config/FirebaseConfig';
+import { useUser } from '@clerk/clerk-expo';
 
 const ChatScreen = () => {
+    const params=useLocalSearchParams();
+    const {user}=useUser();
+    
+    useEffect(()=>{
+        GetUserDetails();
+    },[])
+
+
+
+    const GetUserDetails=async()=>{
+         const docRef=doc(db,'Chat',params?.id);
+         const docSnap=await getDoc(docRef);
+
+         const result=docSnap.data();
+         console.log("result",result)
+         const otherUser=result?.users.filter(item=>item.email!=user?.primaryEmailAddress?.emailAddress)
+         console.log("otherUser",otherUser)
+    }
   return (
     <View>
       <Text>ChatScreen</Text>

81d405d60f465a7da534c75861fda1a1a1993b5c
emresen20 <esen20@posta.pau.edu.tr>
2024-08-26 11:54:14 +0300

    chat initilazie func

--- PetAdopt-app/app/pet-detail/index.jsx
+++ PetAdopt-app/app/pet-detail/index.jsx
@@ -1,16 +1,21 @@
 import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect } from 'react'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { useLocalSearchParams, useNavigation, useRouter } from 'expo-router'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import PetInfo from '../../components/PetDetails/PetInfo';
 import PetSubInfo from '../../components/PetDetails/PetSubInfo';
 import PetAbout from '../../components/PetDetails/PetAbout';
 import PetOwner from '../../components/PetDetails/PetOwner';
 import Colors from '../../constants/Colors';
+import { useUser } from '@clerk/clerk-expo';
+import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
+import { db } from '../../config/FirebaseConfig';
 
 const PetDetails = () => {
   const pet = useLocalSearchParams() // coming from pet listiem router 
   const navigation = useNavigation();
+  const { user } = useUser();
+  const router = useRouter();
 
   useEffect(() => {
     navigation.setOptions({
@@ -18,6 +23,47 @@ const PetDetails = () => {
       headerTitle: ''
     })
   }, [])
+
+  // used tı initiate the chat between two users.
+
+  const InitiateChat = async () => {
+    const docId1 = user?.primaryEmailAddress?.emailAddress + '_' + pet?.email;
+    const docId2 = pet?.email + '_' + user?.primaryEmailAddress?.emailAddress;
+
+    const q = query(collection(db, 'Chat'), where('id', 'in', [docId1, docId2]));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach(doc => {
+      console.log(doc.data())
+      router.push({
+        pathname: "/chat",
+        params: { id: doc.id }
+      })
+    })
+
+    if (querySnapshot.docs?.length == 0) {
+      await setDoc(doc(db, 'Chat', docId1), {
+        id: docId1,
+        users: [
+          {
+            email: user?.primaryEmailAddress?.emailAddress,
+            imageUrl: user?.imageUrl,
+            name: user?.fullName
+
+          },
+          {
+            email: pet?.email,
+            imageUrl: pet?.userImage,
+            name: pet?.username
+          }
+        ]
+      });
+      router.push({
+        pathname: "/chat",
+        params: { id: docId1 }
+      })
+    }
+  }
 
   return (
     <View>
@@ -39,7 +85,9 @@ const PetDetails = () => {
 
       {/* Adopt me button */}
       <View style={styles.buttomcontainer}>
-        <TouchableOpacity style={styles.adoptButton}>
+        <TouchableOpacity
+          onPress={InitiateChat}
+          style={styles.adoptButton}>
           <Text style={styles.adopttext}>Adopt Me</Text>
         </TouchableOpacity>
       </View>
@@ -57,9 +105,9 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.PRIMARY
   },
   buttomcontainer: {
-    position:"absolute",
-    width:'100%',
-    bottom:hp('0%')
+    position: "absolute",
+    width: '100%',
+    bottom: hp('0%')
 
   },
   adopttext: {
